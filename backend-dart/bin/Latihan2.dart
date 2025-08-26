2b75139fb5a4450d96180f5382b86787c3cf1994
bayu <bayupurnamadwiputra@gmail.com>
2025-08-26 12:55:56 +0900

    latihan2

---
+++ backend-dart/bin/Latihan2.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  double Panjang = 10;
+  double Lebar = 5;
+  double Luas = Panjang * Lebar;
+
+  double panjangKeliling = 2 * (Panjang + Lebar);
+
+  print("panjang = $Panjang");
+  print("Lebar = $Lebar");
+  print("Luas = $Luas");
+  print("Panjang Keliling = $panjangKeliling");
+
+}
---
+++ backend-dart/bin/List.dart
@@ -0,0 +1,13 @@
+void main(List<String>args){
+
+  List<String> buah = ['apel', 'jeruk', 'mangga', 'pisang'];
+
+  print(buah[0]);
+  print(buah[1]);
+  print(buah[2]);
+  print(buah[3]);
+  print(buah.first);
+  buah.addAll(['semangka','durian']);
+  print(buah.indexOf(buah[0] = 'apel'));
+  print(buah);
+}
